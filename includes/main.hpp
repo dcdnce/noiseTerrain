c24726ffa67088647db24c5506e4a67cb746ef12
dcdnce <pforesti@student.42nice.fr>
2022-11-05 14:18:14 +0100

    🗺   FastNoiseLite library added

--- includes/main.hpp
+++ includes/main.hpp
@@ -2,10 +2,14 @@
 #define MAIN_HPP
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
 #include "raylib.h"
+#include "FastNoiseLite.h"
 #include "myRaylib.hpp"
 
-#define SCREEN_WIDTH	800
-#define SCREEN_HEIGHT	800
+#define SCREEN_WIDTH	700
+#define SCREEN_HEIGHT	700
 
 #endif // MAIN_HPP
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,14 +1,9 @@
 #include "raylib.h"
 #include "main.hpp"
 
-/*
-void	keyEvents(Camera3D	& camera) {
-	if (IsKeyDown == KEY_W)
-		camera.position.
-	if (IsKeyDown == KEY_D)
-	if (IsKeyDown == KEY_S)
-	if (IsKeyDown == KEY_A)
-}*/
+static float lerp(const float lo, const float hi, const float t) {
+	return (lo * (1 - t) + hi * t);
+}
 
 int main(void)
 {
@@ -16,46 +11,59 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib_test");
     SetTargetFPS(60);
 
+	const int 	width = 100;
+	const int 	height = 100;
+	const int 	scl = 1;
+	const float	minHeight = 0;
+	const float	maxHeight = width / 10;
+
+	// Create and configure FastNoise object
+	FastNoiseLite		noise;
+	noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+	// Map configuration
+	Vector3 map[height * width];
+	Vector3	v[4];
+	for (int y = 0 ; y < height; y++)
+		for (int x = 0 ; x < width; x++)
+			map[(y*height)+x] = {
+					(float)x * scl, 
+					lerp(minHeight, maxHeight, (float)noise.GetNoise((float)x, (float)y)), 
+					(float)y * scl
+			};
+
+	// Camera configuration
 	Camera3D camera = {0};
-	camera.position = (Vector3){0,10,10};
-	camera.target = (Vector3){0,0,0};
+	camera.position = map[height*width-1];
+	camera.position.y = (float)width;
+	camera.target = map[5*height+width/2];
 	camera.up = (Vector3){0,1,0};
 	camera.fovy = 45.0f;
 	camera.projection = CAMERA_PERSPECTIVE;
 	SetCameraMode(camera, CAMERA_FREE);
 
-	const int cols = 10;
-	const int rows = 10;
-	const int scl = 10;
-
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
 	{
 		UpdateCamera(&camera);
-
 		// Draw ----------------
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
-
 			BeginMode3D(camera);
-				Vector3 map[rows * cols];
-				for (int y = 0 ; y < rows; y++) {
-					for (int x = 0 ; x < cols; x++) {
-						map[(y*rows)+x] = (Vector3){(float)x * scl, (float)GetRandomValue(0, 10), (float)y * scl};
-						/*
-						DrawTriangleStrip3D(v, 4, YELLOW);
+				for (int y = 0 ; y < height - 1; y++) {
+					for (int x = 0 ; x < width - 1; x++) {
+						v[0] = map[(y * height)+x];
+						v[1] = map[(y * height)+x+1];
+						v[2] = map[((y+1) * height)+x];
+						v[3] = map[((y+1) * height)+x+1];
 						DrawLine3D(v[0], v[1], BLACK);
-						DrawLine3D(v[0], v[2], BLACK);
-						DrawLine3D(v[3], v[1], BLACK);
+						DrawLine3D(v[1], v[3], BLACK);
 						DrawLine3D(v[3], v[2], BLACK);
-						DrawLine3D(v[3], v[0], BLACK);
-						*/
+						DrawLine3D(v[2], v[0], BLACK);
+						DrawLine3D(v[2], v[1], BLACK);
+						//DrawTriangleStrip3D(v, 4, BLUE);
 					}
 				}
-				DrawTriangleStrip3D(map, rows * cols, BLUE);
-				DrawGrid(100, 1);
 			EndMode3D();
-
 			DrawFPS(10,10);
 		EndDrawing();
 		//----------------
@@ -63,7 +71,6 @@ int main(void)
 
     // De-Initialization
     CloseWindow();        // Close window and OpenGL context
-
-    return 0;
+    return (0);
 }
 
